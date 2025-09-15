d0a7997bf71aa10e118b7f0ab369e2135ba60919
spaa <alf11.spalt@gmx.at>
2025-09-15 09:23:20 +0200

    add_something #2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿Console.WriteLine("jetzt funktioniert es");
 Console.WriteLine("#2");
+
+Console.WriteLine("Version #1");
+Console.WriteLine("add_something #1");
+Console.WriteLine("add_something #2");
