2af2d48b99a1dcd5ddf11dcf96ee1f4d0eac19e9
spaa <alf11.spalt@gmx.at>
2025-09-16 16:12:09 +0200

    ff-test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿Console.WriteLine("jetzt funktioniert es");
-Console.WriteLine("#2");
-
-Console.WriteLine("Version #1");
-Console.WriteLine("add_something #1");
-Console.WriteLine("add_something #2");
+﻿Console.WriteLine("ff-test");
